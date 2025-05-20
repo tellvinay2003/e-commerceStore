8b4639ccad57e7193cdc82c60f291fc5ac91463c
tellvinay2003 <tellvinay2003@yahoo.co.in>
2025-05-20 13:32:24 +0530

    Committing .net8 API with EF configuration

--- eCommerce/APIs/Program.cs
+++ eCommerce/APIs/Program.cs
@@ -1,13 +1,31 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+# region Service region
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// add db context with options for SqlLite
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion Service region
+
+
 var app = builder.Build();
+
+#region Middleware Region
+
+// Middllewarw region Start 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +34,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// middleware sequence 1
 app.UseHttpsRedirection();
 
+// middleware sequence 2
+// sets HttpContext.User 
+app.UseAuthentication();
+
+// middleware sequence 3
+// ensures policy checks
 app.UseAuthorization();
 
+// middleware sequence 4
+// handle controller routes
 app.MapControllers();
 
+// middleware sequence 5
 app.Run();
+
+#endregion Middleware
+
+
